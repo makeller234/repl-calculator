b977ea725298f51bf4ad965598b4cdf344ebb57f
Melissa Keller <melissa.keller23@gmail.com>
2021-09-29 15:13:49 -0500

    Fixed the tokens length problem

--- calculator.py
+++ calculator.py
@@ -6,11 +6,40 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 # Replace this with your code
 # assign a variable to each item in the list
-input_string = 'pow 3 5'
-token = input_string.split(' ')
-func_name = token[0]
-param_a = token[1]
-param_b = token[2]
+
+
+#print(func_name, param_a, param_b)
+
+while True:
+    user_input= input("Enter math function and two numbers")
+    token = user_input.split(' ')
+    func_name = token[0]
+    if len(user_input)>=2:
+        param_a= float(token[1])
+    if len(user_input)>=3:
+        param_b= float(token[2])
+
+        
+    if func_name == "q":
+        break
+
+    elif func_name == "+":
+        print(add(param_a, param_b))
+
+    elif func_name == "-":
+        print(subtract(param_a, param_b))
+    elif func_name == "*":
+        print(multiply(param_a, param_b))
+    elif func_name == "/":
+        print(divide(param_a, param_b))
+    elif func_name == "square":
+        print(square(param_a))
+    elif func_name == "cube":
+        print(cube(param_a))
+    elif func_name == "power":
+        print(add(param_a, param_b))
+    elif func_name == "mod":
+        print(add(param_a, param_b))
 
 # pseudo-code
 # repeat forever:
