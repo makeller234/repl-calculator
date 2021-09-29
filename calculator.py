42ae586870cf846ce7a04595c641fd495ed10154
Melissa Keller <melissa.keller23@gmail.com>
2021-09-29 14:46:29 -0500

    fixed the arithmetic file

--- calculator.py
+++ calculator.py
@@ -5,3 +5,20 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+# assign a variable to each item in the list
+input_string = 'pow 3 5'
+token = input_string.split(' ')
+func_name = token[0]
+param_a = token[1]
+param_b = token[2]
+
+# pseudo-code
+# repeat forever:
+#     read input
+#     tokenize input
+#         if the first token is "q":
+#             quit
+#         else:
+#             (decide which math function to call based on first token)
+#             if the first token is 'pow':
+#                   call the power function with the other two tokens
